ed25893493934c777b4917d48dfb7e986f048a61
Veniamin <Weniaminwelichko@ukr.net>
2020-08-27 14:42:20 +0300

    Complete second task

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,131 +1,80 @@
-'use strict'
+"use strict";
 
-class Array{
-    constructor(...args){
-        this.length = 0;
-        for (let i = 0; i < args.length; i++) {
-          this[this.length] = args[i];
-          this.length++;
-        }    
+class RangeValidator {
+  constructor(from, to) {
+    if (typeof from !== 'number' || typeof to !== 'number') {
+      console.log(from instanceof Number);
+      throw new TypeError("Value must be a number");
     }
 
+    if (!Number.isInteger(from) || !Number.isInteger(to)) {
+      throw new RangeError("Value mast be an integer");
+    }
 
-    push(...args) {
-        for (let i = 0; i < args.length; i++) {
-          this[this.length++] = args[i];
-          return this.length;
-        }
-      };
-    
-      pop() {
-        const lastIndex = this.length - 1;
-        const lastItem = this[lastIndex];
-    
-        delete this[lastIndex];
-    
-        --this.length;
-    
-        return lastItem;
-      };
-    
-      forEach(callback) {
-        for (let i = 0; i < this.length; i++) {
-          callback(this[i], i, this);
-        }
-      };
-    
-      concat(array){
-        let result = new MyArray();
-    
-        for (let i = 0; i < this.length; i++) {
-          result.push(this[i]);
-        }
-    
-        for (let i = 0; i < array.length; i++) {
-          result.push(array[i]);
-        }
-    
-        return result;
-      };
-    
-      flat(depth = 1){
-        if (depth < 0) {
-          console.error("depth must be a positive value");
-          return;
-        }
-    
-        let newArr = new MyArray();
-    
-        if (depth === 0) {
-          return this;
-        }
-    
-        for (let i = 0; i < this.length; i++) {
-          if (Array.isArray(this[i])) {
-    
-            const buffer = this[i].flat(depth - 1);
-    
-            newArr = newArr.concat(buffer);
-    
-          } else if (this[i] !== undefined) {
-    
-            newArr.push(this[i]);
-    
-          }
-        }
-    
-        return newArr;
-      };
+    if (from > to) {
+      throw new RangeError("The second value must be greater than the first");
+    }
 
-      unshift(...args){
-        let oldPartArray = new Array();
-        // debugger;
+    this._from = from;
+    this._to = to;
+  }
 
-        for(let i = 0; i < this.length; i++){
-            oldPartArray[i] = this[i];
-            oldPartArray.length++;
-        }
+  get from() {
+    return this._from;
+  }
 
-        this.length = 0;
+  set from(from) {
+    if (typeof from !== 'number') {
+      throw new TypeError("Value must be a number");
+    }
 
-        for(let i = 0; i < args.length; i++){
-            this[i] = args[i];
-            this.length++;
-        }
+    if (!Number.isInteger(from)) {
+      throw new RangeError("Value mast be an integer");
+    }
 
-        for(let i = args.length, j = 0; i < (args.length + oldPartArray.length); i++, j++){
-            this[i] = oldPartArray[j];
-            this.length++;
-        }
+    if (from > this.to) {
+      throw new RangeError("from must be less that the to");
+    }
 
-        return this.length;
-      }
+    this._from = from;
+  }
 
-      shift(){
-          let lastItem = this[0];
+  get to(){
+    return this._to;
+  }
+  
+  set to(to) {
+    if (typeof to !== 'number') {
+      throw new TypeError("Value must be a number");
+    }
 
-          for(let i = 0; i < this.length; i++){
-              this[i] = this[i + 1];
-          }
-          
-          this.length--;
+    if (!Number.isInteger(to)) {
+      throw new RangeError("Value mast be an integer");
+    }
 
-          return lastItem;
-      }
-      
+    if (this.from > to) {
+      throw new RangeError("from must be less that the to");
+    }
+
+    this._to = to;
+  }
+
+
+  get range() {
+    return [this.from, this.to];
+  }
+
+  validate(value){
+    return (this.from <= value && this.to > value);
+  }
 }
 
-let array = new Array(10, 'test', 'test2');
+const num1 = 10, num2 = 100;
 
-console.log('.unshift() method:');
+let rv = new RangeValidator(10, 100);
 
-console.log(array);
-console.log('Array.unshift(101, 2,\'test0\'), return length:', array.unshift(101, 2,'test0'));
-console.log(array);
-console.log('\n');
+console.log('An instance of the RangeValidation class with the value from = 10, to = 100:', rv);
 
-console.log('.shift() method:');
-
-console.log(array.shift());
-console.log(array);
- 
+console.log('Return value of rv.range:',rv.range);
+console.log('The return value of the validate(50) method is:',rv.validate(50));
+console.log('The return value of the validate(1) method is:',rv.validate(1));
