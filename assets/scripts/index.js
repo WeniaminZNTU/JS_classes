2d5e25faa098b8205f9f9fc68aad56714fd7961e
Veniamin <Weniaminwelichko@ukr.net>
2020-08-27 22:06:15 +0300

    Completed remaining tasks for function(3, 4, 5)

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,80 +1,48 @@
 "use strict";
 
-class RangeValidator {
-  constructor(from, to) {
-    if (typeof from !== 'number' || typeof to !== 'number') {
-      console.log(from instanceof Number);
-      throw new TypeError("Value must be a number");
-    }
-
-    if (!Number.isInteger(from) || !Number.isInteger(to)) {
-      throw new RangeError("Value mast be an integer");
-    }
-
-    if (from > to) {
-      throw new RangeError("The second value must be greater than the first");
-    }
-
-    this._from = from;
-    this._to = to;
-  }
-
-  get from() {
-    return this._from;
-  }
-
-  set from(from) {
-    if (typeof from !== 'number') {
-      throw new TypeError("Value must be a number");
-    }
-
-    if (!Number.isInteger(from)) {
-      throw new RangeError("Value mast be an integer");
-    }
-
-    if (from > this.to) {
-      throw new RangeError("from must be less that the to");
-    }
-
-    this._from = from;
-  }
-
-  get to(){
-    return this._to;
-  }
-  
-  set to(to) {
-    if (typeof to !== 'number') {
-      throw new TypeError("Value must be a number");
-    }
-
-    if (!Number.isInteger(to)) {
-      throw new RangeError("Value mast be an integer");
-    }
-
-    if (this.from > to) {
-      throw new RangeError("from must be less that the to");
-    }
-
-    this._to = to;
-  }
-
-
-  get range() {
-    return [this.from, this.to];
-  }
-
-  validate(value){
-    return (this.from <= value && this.to > value);
-  }
+//Task 3
+function isThere(array, item){
+  return array.includes(item);
 }
 
-const num1 = 10, num2 = 100;
+console.log('Task 3:')
+console.log('The isThere([10,11,12], 10) function works:', isThere([10,11,12], 10));
+console.log('The isThere([10,11,12], \'10\') function works:', isThere([10,11,12], '10'));
+console.log('The isThere([10,11,12], 100) function works:', isThere([10,11,12], 100));
+console.log('\n');
 
-let rv = new RangeValidator(10, 100);
 
-console.log('An instance of the RangeValidation class with the value from = 10, to = 100:', rv);
+//Task 4
+function sumOfDigits(number){
+  number += '';
+  let arr = number.split('');
+  let sum = arr.map(item => item = Number(item)).reduce((accumulator, currentValue) => accumulator + currentValue);
 
-console.log('Return value of rv.range:',rv.range);
-console.log('The return value of the validate(50) method is:',rv.validate(50));
-console.log('The return value of the validate(1) method is:',rv.validate(1));
+  for(let i = 0; sum < 0 || sum > 9; i++){
+    sum = sumOfDigits(sum);
+  }
+  return sum;
+}
+
+console.log('Task 4:')
+console.log('The sumOfDigits(777) function works:', sumOfDigits(777));
+console.log('The sumOfDigits(1333) function works:', sumOfDigits(1333));
+console.log('\n');
+
+
+
+function getUniqueValues(...args){
+  const ArrUniqValues = [];
+
+  for(const item of args){
+    let buffer = new Set(item);
+    ArrUniqValues.push(...buffer)
+  }
+
+  return ArrUniqValues;
+}
+
+let result = getUniqueValues([10, 10, 20, 1, 'test','test', 5],[2, 2, 0,'test3', 1, 'test3']);
+
+console.log('Task 5:');
+console.log('result of the function getUniqueValues([10, 10, 20, 1, \'test\',\'test\', 5],[2, 2, 0,\'test3\', 1, \'test3\']):', result);
